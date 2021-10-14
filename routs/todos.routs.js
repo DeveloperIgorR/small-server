db313eb1e62059b67eb0a7a4277b691fed0f479b
DeveloperIgorR <bratca@inbox.ru>
2021-10-15 02:32:38 +0300

    mongoose  fix add

--- routs/todos.routs.js
+++ routs/todos.routs.js
@@ -7,7 +7,7 @@ router.post('/', async (req,res) => {
     try {
         const{title, completed} = req.body
         const tasks = await Tasks.create({title, completed})
-        res.status(200).json('Ну и запросы у вас')
+        res.json(tasks)
         
     }
     catch (e) {
