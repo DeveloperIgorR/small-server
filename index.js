da37fb3aebd4abf6c13463ba222cb65f29fd5e4a
DeveloperIgorR <bratca@inbox.ru>
2021-10-16 23:28:51 +0300

    set env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 require('dotenv').config()
-const port = process.env.PORT || 3000
+const port = process.env.PORT||3000
 
 const DB_URL = `mongodb+srv://user:user@cluster0.i5vcb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const app = express()
